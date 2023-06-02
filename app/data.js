ec4bd90676f050a16978f6a9a562bc6780ce697f
unknown <tuluhaileyesus2121@gmail.com>
2023-06-02 11:08:56 -0700

    initial commit

---
+++ app/data.js
@@ -0,0 +1,35 @@
+export const quiz = {
+    totalQuestions: 5,
+    questions: [
+      {
+        id: 1,
+        question: 'What is the capital of Ethiopia?',
+        answers: ['kolfe', 'abyssinia', 'Addisabeba', 'piyyassa'],
+        correctAnswer: 'Addisabeba',
+      },
+      {
+        id: 2,
+        question: 'What is the name of the ancient Ethiopian script?',
+        answers: ["Hieroglyphics", "Cuneiform", "Ge'ez", "Phoenician"],
+        correctAnswer: "Ge'ez",
+      },
+      {
+        id: 3,
+        question: 'What is the name of the ancient Ethiopian instrument that is similar to a lyre?',
+        answers: ["Masinko", "Krar", "Washint", "Kebero"],
+        correctAnswer: "Krar",
+      },
+      {
+        id: 4,
+        question: 'which Queen pay a visit for king solomon of Jersusalem ?',
+        answers: ["Queen Nefertiti", "Queen Cleopatra", "Queen Sheba", "Queen Hatshepsut"],
+        correctAnswer:  "Queen Sheba",
+      },
+      {
+        id: 5,
+        question: 'Who is the founder of this quiz web?',
+        answers: ['Steve Jobs', 'Haile Wells', 'Elon Musk', 'Mark Zuckerberg'],
+        correctAnswer: 'Haile Wells',
+      },
+    ],
+  };
